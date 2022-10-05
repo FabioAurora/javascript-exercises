70a7af3554c3db97be14e92d8e707f039131e842
Fabio Aurora <fabioauroradev@gmail.com>
2022-10-05 21:55:22 +0100

    first challenge on palindromes completed
    works with single words

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
 const palindromes = function (string) {
-    
+    const lowerString = string.toLowerCase().replace(/[^a-z0-9]/g, '');
+    return (
+        lowerString.split('').reverse().join('') === lowerString
+    );
 };
 
 // Do not edit below this line
