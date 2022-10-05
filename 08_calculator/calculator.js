63a190d800c20a7501692ea45b77e581bda2c136
Fabio Aurora <fabioauroradev@gmail.com>
2022-10-05 21:31:40 +0100

    completed power exercise in calculator exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -14,8 +14,8 @@ const multiply = function(num) {
   return num.reduce((a,b) => a * b);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return Math.pow(a, b);
 };
 
 const factorial = function() {
