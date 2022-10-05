c5785eaad4fada401b1f63d260b60812fe2599e1
Fabio Aurora <fabioauroradev@gmail.com>
2022-10-05 21:41:07 +0100

    completed challenges in calculator exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -18,8 +18,15 @@ const power = function(a, b) {
 	return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if(num === 0 || num === 1) {
+    return 1;
+  }
+
+  for(let i = num -1; i >= 1; i--) {
+    num = num * i
+  }
+  return num;
 };
 
 // Do not edit below this line
