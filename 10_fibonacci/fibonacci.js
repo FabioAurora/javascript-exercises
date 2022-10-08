5a4b8ddb435d9325841105f7978dc1984fc974ce
Fabio Aurora <fabioauroradev@gmail.com>
2022-10-08 13:02:14 +0100

    completed fibonacci challenge
    finished TDD in fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    let n1 = 0;
+    let n2 = 1;
+    let nextTerm;
+    for(let i = 1; i < num; i++) {
+        nextTerm = n1 +n2;
+        n1 = n2;
+        n2 = nextTerm;
+    }
+    if(num < 0) {
+        return `OOPS`;
+    }
+    return n2;
 };
 
 // Do not edit below this line
